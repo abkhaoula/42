487d05c89060ba5088e5885be77515ee29cbe205
abkhaoula <khaoula.abdenouri@gmail.com>
2021-11-09 17:06:50 +0100

    added strcat functions

---
+++ Libft/ft_strlcat.c
@@ -0,0 +1,24 @@
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (dst[i])
+		i++;
+	if (dstsize < i)
+	{
+		while (src[j])
+			j++;
+		return (dstsize + j);
+	}
+	while (i < dstsize - 1 && src[j])
+		dst[i++] = src[j++];
+	dst[i] = '\0';
+	while (src[j++])
+		i++;
+	return (i);
+}
