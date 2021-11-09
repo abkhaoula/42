2c1272f9f9deab24646f18a19d87a9a622055191
abkhaoula <khaoula.abdenouri@gmail.com>
2021-11-09 17:33:04 +0100

    added the tolower function

--- Libft/libft.h
+++ Libft/libft.h
@@ -16,5 +16,6 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
