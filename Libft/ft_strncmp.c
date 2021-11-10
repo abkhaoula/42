3aabcca13a779ba194a78e5cc040e3238bcc7dcf
abkhaoula <khaoula.abdenouri@gmail.com>
2021-11-10 11:48:16 +0100

    added ft_strncmp

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -5,18 +5,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while ((i < n) && (s1[i]) && (s2[i]) && (s1[i] == s2[i]))
+	while ((i < n - 1) && (s1[i]) && (s2[i]) && (s1[i] == s2[i]))
 		i++;
-	if (i >= n)
-		return (0);
-	else if (s1[i])
-	{
-		if (s2[i])
-			return (s1[i] - s2[i]);
-		return (1);
-	}
-	else if (s2[i])
-		return (-1);
-	else
-		return (0);
+	if (n > 0)
+		return (s1[i] - s2[i]);
+	return (0);
 }
