33c60306adceb58f6b7a4fb7088c989ce422b88c
abkhaoula <khaoula.abdenouri@gmail.com>
2021-11-13 14:15:10 +0100

    added ft_lstnew function

---
+++ Libft/ft_lstnew_bonus.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kabdenou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/12 16:28:00 by kabdenou          #+#    #+#             */
+/*   Updated: 2021/11/12 16:28:00 by kabdenou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+#include <stdlib.h>
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
