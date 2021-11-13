5b91e49f947f32c2e3e14fec02f119d696e2e890
abkhaoula <khaoula.abdenouri@gmail.com>
2021-11-13 14:49:48 +0100

    added ft_lstlast_bonus function

---
+++ Libft/ft_lstlast_bonus.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kabdenou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/13 16:28:00 by kabdenou          #+#    #+#             */
+/*   Updated: 2021/11/13 16:28:00 by kabdenou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+#include <stdlib.h>
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
