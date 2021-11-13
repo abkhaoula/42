052c5fab5a7c6a10eda3473ac3df0218bf9f0a10
abkhaoula <khaoula.abdenouri@gmail.com>
2021-11-13 15:03:41 +0100

    deleted unused include

--- Libft/ft_lstadd_front_bonus.c
+++ Libft/ft_lstadd_front_bonus.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-#include <stdlib.h>
 
 void	ft_lstadd_front(t_list **alst, t_list *new)
 {
