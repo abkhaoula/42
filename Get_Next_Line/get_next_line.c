e7653e29ee22802083d72fe51337e8fca80a8d28
abkhaoula <khaoula.abdenouri@gmail.com>
2021-11-26 11:59:57 +0100

    initial working get_next_line still need to perform tests on it

---
+++ Get_Next_Line/get_next_line.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kabdenou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 16:28:00 by kabdenou          #+#    #+#             */
+/*   Updated: 2021/11/23 16:28:00 by kabdenou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "get_next_line.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+
+char	*protecte_gnl(int fd)
+{
+	char	*next_buff;
+	int		isnt_eof;
+
+	if ((BUFFER_SIZE <= 0) || (fd == -1))
+		return (NULL);
+	next_buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!next_buff)
+		return (NULL);
+	isnt_eof = read(fd, &next_buff[0], 1);
+	if (isnt_eof <= 0)
+		return (NULL);
+	if (!isnt_eof)
+		return (ft_strdup("\0"));
+	if (next_buff[0] == '\n')
+		return (ft_strdup("\n"));
+	return (next_buff);
+}
+
+int	next_stop(int fd, char **next_buff, int i)
+{
+	int		isnt_eof;
+	char	tmp;
+
+	isnt_eof = read(fd, &tmp, 1);
+	if (!isnt_eof)
+	{
+		(*next_buff)[i] = '\0';
+		return (1);
+	}
+	(*next_buff)[i] = tmp;
+	if ((*next_buff)[i] == '\n')
+	{
+		(*next_buff)[i + 1] = '\0';
+		return (1);
+	}
+	return (0);
+}
+
+char	*get_next_line(int fd)
+{
+	char	*next_buff;
+	char	*next_line;
+	int		i;
+	int		stop;
+
+	next_buff = protecte_gnl(fd);
+	if ((!next_buff) || (next_buff[0] == '\n') || (next_buff[0] == '\0'))
+		return (next_buff);
+	next_line = "";
+	i = 1;
+	stop = 0;
+	while (1)
+	{
+		while (i < BUFFER_SIZE && (!stop))
+		{
+			stop = next_stop(fd, &next_buff, i);
+			i++;
+		}
+		next_line = ft_strjoin(next_line, next_buff);
+		if (stop)
+			return (next_line);
+		i = 0;
+	}
+	next_buff[i] = '\0';
+	return (next_buff);
+}
---
+++ Get_Next_Line/get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kabdenou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 16:28:00 by kabdenou          #+#    #+#             */
+/*   Updated: 2021/11/23 16:28:00 by kabdenou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
