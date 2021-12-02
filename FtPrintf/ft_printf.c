d53552709c4afcfe4e527fef45e82230c4df6ef0
abkhaoula <khaoula.abdenouri@gmail.com>
2021-12-02 13:40:42 +0100

    almost norminetted

--- FtPrintf/ft_printf.c
+++ FtPrintf/ft_printf.c
@@ -47,29 +47,27 @@ size_t	ft_strlen(const char *s)
 	return (i);
 }
 
-void	ft_putchar(char c, int *count, int *i)
+void	ft_putchar(char c, int *count)
 {
 	write(1, &c, 1);
-	(*i)++;
 	(*count)++;
 }
 
-void	ft_putstr(char *s, int *count, int *i)
+void	ft_putstr(char *s, int *count)
 {
 	if (!s)
 	{
 		write(1, "(null)", 6);
-        (*count) = (*count) + 6;
+		(*count) = (*count) + 6;
 	}
-    else
-    {
-        write(1, s, ft_strlen(s));
-        (*count) = (*count) + ft_strlen(s);
-    }
-    (*i)++;
+	else
+	{
+		write(1, s, ft_strlen(s));
+		(*count) = (*count) + ft_strlen(s);
+	}
 }
 
-void hex_digit_write(int num, int islower)
+void	hex_digit_write(int num, int islower)
 {
 	char	w;
 	char	a;
@@ -86,32 +84,30 @@ void hex_digit_write(int num, int islower)
 
 void	hex_div(unsigned long int s, int *count, int islower)
 {
-    if (s < 16)
-        hex_digit_write(s, islower);
-    else
-    {
-        hex_div(s/16, count, islower);
-        hex_digit_write((s%16), islower);
-    }
-    (*count)++;
+	if (s < 16)
+		hex_digit_write(s, islower);
+	else
+	{
+		hex_div(s / 16, count, islower);
+		hex_digit_write((s % 16), islower);
+	}
+	(*count)++;
 }
 
-void	put_adress(unsigned long int s, int *count, int *i)
+void	put_adress(unsigned long int s, int *count)
 {
 	if (!s)
 	{
-        write(1, "(nil)", 5);
-        (*count) = (*count) + 5;
-        (*i)++;
+		write(1, "(nil)", 5);
+		(*count) = (*count) + 5;
 	}
-    else
-    {
-	    write(1, "0x", 2);
-	    (*count)++;
-	    (*count)++;
-	    hex_div(s, count, 1);
-	    (*i)++;
-    }
+	else
+	{
+		write(1, "0x", 2);
+		(*count)++;
+		(*count)++;
+		hex_div(s, count, 1);
+	}
 }
 
 static int	estim(long n)
@@ -162,7 +158,7 @@ static char	*gen(char *rtn, long nbr, int len, int isneg)
 	return (rtn);
 }
 
-void	ft_itoa(int n, int *count, int *i)
+void	ft_itoa(int n, int *count)
 {
 	int		len;
 	char	*rtn;
@@ -176,7 +172,7 @@ void	ft_itoa(int n, int *count, int *i)
 	rtn = gen(rtn, nbr, len, isneg);
 	if (!rtn)
 		return ;
-	ft_putstr(rtn, count, i);
+	ft_putstr(rtn, count);
 	free(rtn);
 }
 
@@ -193,12 +189,12 @@ static int	estim_u(unsigned long n)
 	return (estim);
 }
 
-void	ft_itoa_u(unsigned int n, int *count, int *i)
+void	ft_itoa_u(unsigned int n, int *count)
 {
-	int		len;
-	char	*rtn;
+	int				len;
+	char			*rtn;
 	unsigned long	nbr;
-	int		isneg;
+	int				isneg;
 
 	nbr = n;
 	len = estim_u(nbr);
@@ -207,77 +203,58 @@ void	ft_itoa_u(unsigned int n, int *count, int *i)
 	rtn = gen(rtn, nbr, len, isneg);
 	if (!rtn)
 		return ;
-	ft_putstr(rtn, count, i);
+	ft_putstr(rtn, count);
 	free(rtn);
 }
 
-void    conversions(const char *message, int *i, va_list arguments, int *count)
+void	conversions(const char *message, va_list arguments, int *count, int i)
 {
-	if (message[(*i)+1] == 'c')
-	{
-		ft_putchar(va_arg(arguments, int), count, i);
-	}
-	else if (message[(*i)+1] == 's')
-	{
-		ft_putstr(va_arg(arguments, char*), count, i);
-	}
-	else if (message[(*i)+1] == 'p')
-	{
-		put_adress(va_arg(arguments, unsigned long int), count, i);
-	}
-	else if (message[(*i)+1] == 'd')
-	{
-		ft_itoa(va_arg(arguments, int), count, i);
-	}
-	else if (message[(*i)+1] == 'i')
-	{
-		ft_itoa(va_arg(arguments, int), count, i);
-	}
-	else if (message[(*i)+1] == 'u')
-	{
-		ft_itoa_u(va_arg(arguments, unsigned int), count, i);
-	}
-	else if (message[(*i)+1] == 'x')
-	{
+	if (message[i + 1] == 'c')
+		ft_putchar(va_arg(arguments, int), count);
+	else if (message[i + 1] == 's')
+		ft_putstr(va_arg(arguments, char *), count);
+	else if (message[i + 1] == 'p')
+		put_adress(va_arg(arguments, unsigned long int), count);
+	else if (message[i + 1] == 'd')
+		ft_itoa(va_arg(arguments, int), count);
+	else if (message[i + 1] == 'i')
+		ft_itoa(va_arg(arguments, int), count);
+	else if (message[i + 1] == 'u')
+		ft_itoa_u(va_arg(arguments, unsigned int), count);
+	else if (message[i + 1] == 'x')
 		hex_div(va_arg(arguments, unsigned), count, 1);
-		(*i)++;
-	}
-	else if (message[(*i)+1] == 'X')
-	{
+	else if (message[i + 1] == 'X')
 		hex_div(va_arg(arguments, unsigned), count, 0);
-		(*i)++;
-	}
-	else if (message[(*i)+1] == '%')
+	else if (message[i + 1] == '%')
 	{
 		write(1, "%", 1);
-		(*i)++;
 		(*count)++;
 	}
 }
-
 
 int	ft_printf(const char *message, ...)
 {
 	int		count;
 	va_list	arguments;
-    int i;
+	int		i;
 
-    i = 0;
-    count = 0;
+	i = 0;
+	count = 0;
 	va_start(arguments, message);
-    while(message[i])
-    {
-        if (message[i] == '%')
-        {
-			conversions(message, &i, arguments, &count);	
+	while (message[i])
+	{
+		if (message[i] == '%')
+		{
+			conversions(message, arguments, &count, i);
+			i++;
 		}
-        else
-        {
-            write(1, &message[i], 1);
-            count++;
-        }
-        i++;
-    }
+		else
+		{
+			write(1, &message[i], 1);
+			count++;
+		}
+		i++;
+	}
 	va_end(arguments);
 	return (count);
 }
