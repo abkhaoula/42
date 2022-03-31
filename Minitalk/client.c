2ec576552f512cedcc6af284624bfca520a5c6e2
abkhaoula <khaoula.abdenouri@gmail.com>
2022-03-31 08:58:09 +0000

    looks like it worked

--- Minitalk/client.c
+++ Minitalk/client.c
@@ -12,7 +12,17 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include "client.h"
+
+void	handler(int signum)
+{
+	if (signum == SIGUSR1)
+	{
+		printf("received haha \n");
+		exit(-1);
+	}
+}
 
 void	stob_sig(int pid, char *message)
 {
@@ -41,14 +51,47 @@ void	stob_sig(int pid, char *message)
 	}
 }
 
+void	itob_sig(int pid, int n)
+{
+	int i;
+
+	i = 0;
+	while(n)
+	{
+		if (n%2)
+		{
+			kill(pid, SIGUSR1);
+			usleep(0.5);
+		}
+		else
+		{
+			kill(pid, SIGUSR2);
+			usleep(0.5);
+		}
+		n = (n-n%2)/2;
+		i++;
+	}
+	while (i < 17)
+	{
+		kill(pid, SIGUSR2);
+		usleep(0.5);
+		i++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	int		pid;
-	char	*message;
+	int		server_pid;
+	pid_t	client_pid;
 
 	if (argc != 3)
 		return (0);
-	pid = ft_atoi(argv[1]);
-	message = argv[2];
-	stob_sig(pid, message);
+	client_pid = getpid();
+	server_pid = ft_atoi(argv[1]);
+	itob_sig(server_pid, client_pid);
+	stob_sig(server_pid, argv[2]);
+	while(1)
+	{
+		signal(SIGUSR1, handler);
+	}
 }
--- Minitalk/server.c
+++ Minitalk/server.c
@@ -14,7 +14,7 @@
 #include <unistd.h>
 #include <stdlib.h> 
 
-int	g_num[15];
+int	g_num[20];
 
 void	handler1(int signum)
 {
@@ -57,44 +57,78 @@ void	decode(void)
 	g_num[0] = 1;
 }
 
+int	decode_pid(void)
+{
+	int	i;
+	int	pid;
+
+	i = 17;
+	pid = 0;
+	while(i > 0)
+	{
+		pid = (pid * 2) + g_num[i];
+		i--;
+	}
+	return (pid);
+}
+
 int	main(void)
 {
+	int		i;
 	pid_t	pid;
 	int		timer;
-	int		send;
+	int		client_pid;
 
+	client_pid = 0;
 	g_num[0] = 1;
 	g_num[-1] = -1;
 	timer = 0;
-	send = 0;
 	pid = getpid();
 	printf("pid: %u\n", pid);
 	while (1)
 	{
-		if (g_num[-1] != -1)
+		if (client_pid == 0)
 		{
-			if (send == 0)
+			signal(SIGUSR1, handler1);
+			if (g_num[0] == 18)
+				client_pid = decode_pid();
+			signal(SIGUSR2, handler2);
+			if (g_num[0] == 18)
+				client_pid = decode_pid();
+			if (client_pid != 0)
 			{
-				printf("\nsending\n");
-				send = 1;
+				g_num[0] = 1;
+				i = 1;
+				while (i < 20)
+				{
+					g_num[i] = 0;
+					i++;
+				}
 			}
-			g_num[-1] = 0;
 		}
-		signal(SIGUSR1, handler1);
-		if (g_num[0] == 9)
-			decode();
-		signal(SIGUSR2, handler2);
-		if (g_num[0] == 9)
-			decode();
+		else
+		{
+			if (g_num[-1] != -1)
+			{
+				g_num[-1] = 0;
+			}
+			signal(SIGUSR1, handler1);
+			if (g_num[0] == 9)
+				decode();
+			signal(SIGUSR2, handler2);
+			if (g_num[0] == 9)
+				decode();
+		}
 		if (g_num[-1] == 0)
 			timer++;
 		else
 			timer = 0;
 		if (timer == 100000)
 		{
-			send = 0;
 			timer = 0;
 			g_num[-1] = -1;
+			kill(client_pid, SIGUSR1);
+			client_pid = 0;
 		}
 	}
 }
