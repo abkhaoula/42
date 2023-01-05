45f578f29bb6c0aab1ec22c6c7854ecfb8534fcf
Khaoula Abdenouri <kabdenou@e1r2p7.1337.ma>
2023-01-05 13:09:28 +0100

    refining

--- Cpp/Cpp01/ex00/Zombie.cpp
+++ Cpp/Cpp01/ex00/Zombie.cpp
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kabdenou <kabdenou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/05 13:08:28 by kabdenou          #+#    #+#             */
+/*   Updated: 2023/01/05 13:08:29 by kabdenou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Zombie.hpp"
 #include <iostream>
 
 Zombie::Zombie(std::string s) {
-    name = s;
+    _name = s;
 }
 Zombie::~Zombie() {
-    std::cout << name << ": " << "Out ..." << std::endl;
+    std::cout << _name << ": " << "Out ..." << std::endl;
 }
 void Zombie::announce( void ) {
-    std::cout << name << ": " << "BraiiiiiiinnnzzzZ..." << std::endl;
+    std::cout << _name << ": " << "BraiiiiiiinnnzzzZ..." << std::endl;
 }
--- Cpp/Cpp01/ex00/Zombie.hpp
+++ Cpp/Cpp01/ex00/Zombie.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kabdenou <kabdenou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/05 13:08:34 by kabdenou          #+#    #+#             */
+/*   Updated: 2023/01/05 13:08:35 by kabdenou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef __ZOMBIE_H__
 #define __ZOMBIE_H__
 
@@ -5,7 +17,7 @@
 
 class Zombie {
     private:
-        std::string name;
+        std::string _name;
     public:
         Zombie(std::string s);
         ~Zombie();
