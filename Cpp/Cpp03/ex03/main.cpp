719e99d11f4ab65a19b86872494161ade2be3213
khaoula <khaoula.abdenouri@gmail.com>
2023-01-09 12:15:41 +0100

    updated the main

--- Cpp/Cpp03/ex03/main.cpp
+++ Cpp/Cpp03/ex03/main.cpp
@@ -1,11 +1,22 @@
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
 #include "FragTrap.hpp"
+#include "DiamondTrap.hpp"
 
-int main() {
-    ClapTrap c("khaoula");
-    FragTrap s("laila");
-    c.attack("v");
-    s.attack("v");
-    s.highFivesGuys();
+int main( void )
+{
+    DiamondTrap a( "A" );
+    DiamondTrap c( ash );
+    DiamondTrap b("B");
+
+    a.whoAmI();
+    c.whoAmI();
+    b = ash;
+    b.whoAmI();
+
+    a.attack( "D" );
+    a.takeDamage( 10 );
+    a.beRepaired( 10 );
+
+    return 0;
 }
