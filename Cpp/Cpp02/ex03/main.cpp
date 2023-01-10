e2c4f25e6a4319c89f355f4b1db45aed2da79dd8
Khaoula Abdenouri <kabdenou@e1r1p4.1337.ma>
2023-01-10 13:23:45 +0100

    deleting asserts

--- Cpp/Cpp02/ex03/main.cpp
+++ Cpp/Cpp02/ex03/main.cpp
@@ -9,14 +9,14 @@ int main()
     Point a(0, 0);
     Point b(1, 0);
     Point c(0, 1);
-    Point d(1/4, 1/4);
+    Point d(1./4, 1./4);
     Point e(1, 1);
     Point f(2, 2);
 
     //bsp(a, b, c, e);
-    assert(bsp(a, b, c, d) == true);  // Point d is inside the triangle
-    assert(bsp(a, b, c, e) == false); // Point e is on the edge of the triangle
-    assert(bsp(a, b, c, f) == false); // Point f is outside the triangle
+    std::cout<<(bsp(a, b, c, d) == true);  // Point d is inside the triangle
+    std::cout<<(bsp(a, b, c, e) == false); // Point e is on the edge of the triangle
+    std::cout<<(bsp(a, b, c, f) == false); // Point f is outside the triangle
 
     return 0;
 }
