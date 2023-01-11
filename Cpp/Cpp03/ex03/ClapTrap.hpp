e0d0e88ea25ff902b4c1fd8ced5f3528c0637f76
Khaoula Abdenouri <kabdenou@e1r1p6.1337.ma>
2023-01-11 15:42:21 +0100

    added virtual destructor

--- Cpp/Cpp03/ex03/ClapTrap.hpp
+++ Cpp/Cpp03/ex03/ClapTrap.hpp
@@ -14,7 +14,7 @@ class ClapTrap {
         ClapTrap(std::string name);
         ClapTrap(const ClapTrap& p);
         ClapTrap& operator=(const ClapTrap& p);
-        ~ClapTrap();
+        virtual ~ClapTrap();
         void attack(const std::string& target);
         void takeDamage(unsigned int amount);
         void beRepaired(unsigned int amount);
