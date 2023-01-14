5daf0a85ff220d83fbb34630cc2f2f1d03ecc40b
Khaoula Abdenouri <kabdenou@e1r1p6.1337.ma>
2023-01-14 12:36:30 +0100

    deep copy

--- Cpp/Cpp04/ex01/Brain.cpp
+++ Cpp/Cpp04/ex01/Brain.cpp
@@ -5,11 +5,13 @@ Brain::Brain() {
     for(int i = 0; i < 100; i++){
         ideas[i] = "Think";
     }
+    std::cout << "Brain created" << std::endl;
 }
 Brain::Brain(const Brain& b) {
     for(int i = 0; i < 100; i++){
         ideas[i] = b.ideas[i];
     }
+    std::cout << "Brain created" << std::endl;
 }
 
 Brain& Brain::operator=(const Brain& b) {
@@ -19,4 +21,5 @@ Brain& Brain::operator=(const Brain& b) {
     return (*this);
 }
 Brain::~Brain() {
+    std::cout << "Brain deleted" << std::endl;
 }
