a56b047ee178d00a863b1cdb0f6dc54877cac306
khaoula <khaoula.abdenouri@gmail.com>
2023-02-14 10:02:18 +0100

    ex01 done

---
+++ Cpp/Cpp07/ex01/iter.hpp
@@ -0,0 +1,14 @@
+#ifndef ITER_HPP
+# define ITER_HPP
+
+#include <iostream>
+#include <string>
+
+template <typename T>
+void iter(T *array, int length, void (*f)(T const &))
+{
+    for (int i = 0; i < length; i++)
+        f(array[i]);
+}
+
+#endif
