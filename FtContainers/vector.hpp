ac2996366c89f2687b1de4745da037a85c82e121
khaoula <khaoula.abdenouri@gmail.com>
2023-03-01 10:01:26 +0100

    reformatting

--- FtContainers/vector.hpp
+++ FtContainers/vector.hpp
@@ -19,41 +19,28 @@ namespace ft {
 
             //
             typedef T														value_type;
-
             //
             typedef Allocator												allocator_type;
-
             //
             typedef size_t													size_type;
-
 			//
 			typedef typename allocator_type::reference 						reference;
-
 			//
 			typedef typename allocator_type::const_reference				const_reference;
-
 			//
 			typedef typename allocator_type::pointer						pointer;
-
 			//
 			typedef typename allocator_type::const_pointer					const_pointer;
-
 			//
 			typedef ft::iterator< T >										iterator;
-
 			//
 			typedef ft::iterator< const T >									const_iterator;
-
 			//
 			typedef typename iterator_traits<iterator>::difference_type		difference_type;
-
 			//
 			typedef ft::reverse_iterator<iterator>							reverse_iterator;
-
 			//
 			typedef ft::reverse_iterator<const_iterator>			const_reverse_iterator;
-
-			//
 
 
 			///CONSTRUCTOR///
